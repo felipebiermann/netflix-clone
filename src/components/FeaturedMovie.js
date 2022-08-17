48e6a1fd4bcd80949fc5e10358bfe681932a0ee9
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-17 18:11:17 -0300

    renderizando serie principal na home

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./FeaturedMovie.css";
 
-export default () => {
-  return <div>FILME EM DESTAQUE</div>;
+export default ({ item }) => {
+  return (
+    <section className="featured">
+      <div>{item.original_name}</div>
+    </section>
+  );
 };
