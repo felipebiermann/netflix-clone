12935bc3e9b073a952ce2874f609bd3d023c519c
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-19 15:23:13 -0300

    netflix clone is done.

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieRow.css";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export default ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(0);
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = items.results.length * 150;
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60;
+    }
+    setScrollX(x);
+  };
   return (
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow--left">
-        <ArrowBackIosIcon style={{ fontSize: 40 }} />
+      <div className="movieRow--left" onClick={handleLeftArrow}>
+        <ArrowBackIosIcon style={{ fontSize: 30 }} />
       </div>
-      <div className="movieRow--right">
-        <ArrowForwardIosIcon style={{ fontSize: 40 }} />
+      <div className="movieRow--right" onClick={handleRightArrow}>
+        <ArrowForwardIosIcon style={{ fontSize: 30 }} />
       </div>
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{ marginLeft: scrollX, width: items.results.length * 150 }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow--item">
