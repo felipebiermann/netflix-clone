1601f9cd8b973317e19af269a89597925e442162
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-19 13:37:20 -0300

    quase no final.

--- src/App.js
+++ src/App.js
@@ -49,6 +49,18 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+        Feito com{" "}
+        <span role="img" aria-label="coração">
+          🖤
+        </span>{" "}
+        por Felipe Biermann
+        <br />
+        Direitos de imagem para Netflix
+        <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   );
 };
