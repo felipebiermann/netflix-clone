8c3292a1eaa7c4528c89d4b78af6aaad73a1ed2a
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-19 14:55:50 -0300

    parte final.

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./MovieRow.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export default ({ title, items }) => {
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow--left">
+        <ArrowBackIosIcon style={{ fontSize: 40 }} />
+      </div>
+      <div className="movieRow--right">
+        <ArrowForwardIosIcon style={{ fontSize: 40 }} />
+      </div>
       <div className="movieRow--listarea">
         <div className="movieRow--list">
           {items.results.length > 0 &&
